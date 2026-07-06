459232086505a7cf01d3b399abd2e3f387345764
Ranya Krichen <raniakrichen07@gmail.com>
2026-07-06 17:40:12 +0100

    Improve application layout with Material navigation

--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    MatListModule,
+    MatIconModule
   ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss'
--- src/app/shared/components/topbar/topbar.ts
+++ src/app/shared/components/topbar/topbar.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
+  imports: [MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './topbar.html',
   styleUrl: './topbar.scss'
 })
